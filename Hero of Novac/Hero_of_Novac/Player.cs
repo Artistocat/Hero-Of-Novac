887ad7fef85e17fdde6c44be4dfaa0ff6c56e6c3
181484 <181484@S020RMD114-23>
2019-03-29 11:41:41 -0500

    thunbsticks maybe

--- Hero of Novac/Hero_of_Novac/Player.cs
+++ Hero of Novac/Hero_of_Novac/Player.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Hero_of_Novac
 {
-    public class Player
+    
+    public class Player : Entity
     {
+        public Texture2D sprites;
+        public Rectangle source;
+        public Rectangle destination;
+        public Color colour;
+        public Vector2 loc;
 
+
+        public Player()
+        {
+
+        }
+
+
+        public override void Update()
+        {
+            GamePadState gps = GamePad.GetState(PlayerIndex.One);
+            if (gps.ThumbSticks.Left.X > 0 || gps.ThumbSticks.Left.Y > 0)
+            {
+                loc = gps.ThumbSticks.Left;
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
     }
 }
