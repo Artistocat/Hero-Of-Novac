f71a986cf9796239d52c54ce4c91494ac2b0bf05
Aaron Wheeler <aaronw502@gmail.com>
2019-03-29 11:40:00 -0500

    Updated Player class to be an Entity Adding BattleState enum to BattleMenu with switch case in update

--- Hero of Novac/Hero_of_Novac/Player.cs
+++ Hero of Novac/Hero_of_Novac/Player.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Hero_of_Novac
 {
-    public class Player
+    public class Player : Entity
     {
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
 
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
